ce78b5f5f26d4cc344d1b99574802429f1f2413c
froiskallico <froiskallico@gmail.com>
2019-10-03 16:50:50 -0300

    add frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -15,7 +17,9 @@ mongoose.connect('mongodb+srv://tri:tritec@tri-cluster0-piuhh.mongodb.net/semana
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
  
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes); 
 
 app.listen(3333);
